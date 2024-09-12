0c915b41d007a05d5deeabe84ef62fe0a2e02282
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-09-12 21:04:11 +0200

    :wrench: init the project   :wrench:

---
+++ src/app.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getUsers() {
+    return [
+      {
+        id: 1,
+        firstName: 'John',
+      },
+    ];
+  }
+}
